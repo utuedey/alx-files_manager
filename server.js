ae2fd1603776a31de2d550b12006f26c5722f3e6
utuedey <utuedeyejosph@gmail.com>
2023-09-03 09:11:44 -0700

    add main files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import { router } from './routes/index';
-const express = require('express')
+
+const express = require('express');
 
 const port = parseInt(process.env.PORT, 10) || 5000;
 const app = express();
